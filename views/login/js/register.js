23333c9493877319d718957811317b737653e944
39253 <392539910@qq.com>
2017-12-12 17:41:16 +0800

    first commit

--- views/login/js/register.js
+++ views/login/js/register.js
@@ -15,18 +15,18 @@ $('.checked').on('click',function(){
 $('.tamWz').on('click',function (){
     $.ajax({
         url:'http://sapi.253.com/msg/HttpBatchSendSM',
-        type:'get',
-        dataType: 'jsonp',
+        type:'post',
+        dataType: 'json',
         data:{
             //account=vip-lsy1&pswd=Tch5832075&mobile=18627798893&msg=您的注册验证码是:aaa&needstatus=true
             account:'vip-lsy1',
             pswd:'Tch5832075',
-            mobile:'15727042308',
-            msg:'您的注册验证码是:aaa',
-            needstatus:true
+            phone:'15727042308',
+            msg:'您的注册验证码是:8767'
+            //needstatus:true
         },
         success:function (data){
-            console.log(1)
+            console.log(data)
         },
         error: function(xhr, ajaxOptions, thrownError) {
             console.log(xhr, ajaxOptions, thrownError)
