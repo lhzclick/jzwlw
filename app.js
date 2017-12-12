776c112f402fd665722cd3e1976965007fcbe911
39253 <392539910@qq.com>
2017-12-12 09:18:51 +0800

    first commit

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ var users = require('./routes/users');
 
 var app = express();
 let mysql =require('mysql');
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -20,18 +19,76 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));  //配置公共静态文件
 
 app.use('/', routes);
 app.use('/users', users);
 app.use('/views', express.static('views'));   //配置项目静态文件
-/// catch 404 and forwarding to error handler
+//mysql创建连接
+var connection = mysql.createConnection({
+    host:'114.135.61.186',
+    user:'root',
+    password:'root',
+    port:'33061',
+    database:'jzwlw'
+})
+//执行连接
+connection.connect(function(err){
+    if(err){
+        console.log('[query]-:'+err);
+    }else{
+        console.log('conneting')
+    }
+})
+
+
+//connection.end();
+//查询
+app.post('/info', function (req, res) {
+    //执行查询
+    let userGetSql = 'SELECT * FROM userInfo';
+    connection.query(userGetSql,function(err,result){
+        if(err){
+            console.log('查询错误')
+        }else{
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Headers", "X-Requested-With");
+            res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+            res.header("X-Powered-By",' 3.2.1')
+            res.header("Content-Type", "application/json;charset=utf-8");
+            res.send(result);
+        }
+    });
+});
+
+
+//注册 添加用户
+app.post('/add',function (req,res){
+    let querys = req.query;
+    let [loginName,password,tel,nickName] = [querys.loginName,querys.password,querys.tel,querys.nickName];
+    console.log(querys)
+    connection.query("insert into info(loginName,password,tel,nickName) values('"+loginName+"','"+ password +"','"+tel+"','"+ nickName +"')",function(err,rows){
+        if(err){
+            res.send("新增失败"+err);
+        }else {
+            res.send("新增成功");
+        }
+    });
+});
+
+//
+
+
+
+
+
+
+// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
-
 /// error handlers
 
 // development error handler
@@ -53,53 +110,6 @@ app.use(function(err, req, res, next) {
     res.render('error', {
         message: err.message,
         error: {}
-    });
-});
-
-
-
-/*---------------------------------------------------后台方法--------------------------------------------------------------*/
-//mysql创建连接
-//var connection = mysql.createConnection({
-//    host:'localhost',
-//    user:'root',
-//    password:'root',
-//    port:'3306',
-//    database:'node'
-//});
-var connection = mysql.createConnection({
-    host:'114.135.61.186',
-    user:'root',
-    password:'root',
-    port:'33061',
-    database:'node'
-})
-//执行连接
-connection.connect(function(err){
-    if(err){
-        console.log('[query]-:'+err);
-    }else{
-        console.log('conneting')
-    }
-})
-
-
-//connection.end();
-//查询
-app.get('/info', function (req, res) {
-    //执行查询
-    let userGetSql = 'SELECT * FROM info';
-    connection.query(userGetSql,function(err,result){
-        if(err){
-            console.log('查询错误')
-        }else{
-            res.header("Access-Control-Allow-Origin", "*");
-            res.header("Access-Control-Allow-Headers", "X-Requested-With");
-            res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-            res.header("X-Powered-By",' 3.2.1')
-            res.header("Content-Type", "application/json;charset=utf-8");
-            res.send(result);
-        }
     });
 });
 
