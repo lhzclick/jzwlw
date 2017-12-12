476202154506cc6d4c2077b8bcb4f54675c715a7
39253 <392539910@qq.com>
2017-12-12 16:04:06 +0800

    first commit

--- views/login/js/register.js
+++ views/login/js/register.js
@@ -16,7 +16,7 @@ $('.tamWz').on('click',function (){
     $.ajax({
         url:'http://sapi.253.com/msg/HttpBatchSendSM',
         type:'get',
-        //dataType:'jsonp',
+        dataType: 'jsonp',
         data:{
             //account=vip-lsy1&pswd=Tch5832075&mobile=18627798893&msg=您的注册验证码是:aaa&needstatus=true
             account:'vip-lsy1',
@@ -27,6 +27,9 @@ $('.tamWz').on('click',function (){
         },
         success:function (data){
             console.log(1)
+        },
+        error: function(xhr, ajaxOptions, thrownError) {
+            console.log(xhr, ajaxOptions, thrownError)
         }
     })
 })
