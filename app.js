a274cee41849e15a642aa4e6f0e36215ac84e8a0
39253 <392539910@qq.com>
2017-12-13 15:13:59 +0800

    first commit

--- app.js
+++ app.js
@@ -169,8 +169,7 @@ app.post('/message', function(req, res){
     var proxy_url = `http://sapi.253.com/msg/HttpBatchSendSM?account=vip-lsy1&pswd=Tch5832075&mobile=${bodyInfo.telNum}&msg=您的注册验证码是:${Num}&needstatus=true`;
     var options = {
         headers: {
-            "Connection": "close",
-            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+            "Connection": "close"
         },
         url: proxy_url,
         method: method,
